e7b95c4a43bd5e9ad6e7ae63d4de73d522848e88
ntthuat <nguyenthienthuat92@gmail.com>
2018-06-29 11:26:23 +0700

    6th commit
    + Add embedded H2 database successfully
    + Insert 2 records, refer h2.sql

--- Report/src/main/java/com/SpringBootViewApplication.java
+++ Report/src/main/java/com/SpringBootViewApplication.java
@@ -14,6 +14,8 @@ public class SpringBootViewApplication
   public static void main(String[] args)
   {
     System.out.println("Check here: http://localhost:8080/");
+    System.out.println("Database here: http://localhost:8080/h2-console");
+
     SpringApplication.run(SpringBootViewApplication.class, args);
   }
 }
